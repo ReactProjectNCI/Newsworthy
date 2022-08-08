79b51f9dfb5b112a6ffe613e1ae2783e3b002b36
webdevKieran <x21205655@student.ncirl.ie>
2022-08-08 12:10:42 +0100

    fixed Signup and added Contribute form

--- src/components/Contribute.js
+++ src/components/Contribute.js
@@ -1,8 +1,29 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
 function Contribute() {
+     
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch["name", "email", "comment"]); 
+  
   return (
     <>
       <p>Contribute</p>
       <p>A form here for suggesting contributions to the website</p>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <p>Firstname</p>
+      <input defaultValue="Firstname" {...register("name")} />
+      <p>Email Address (required)</p>
+      <input defaultValue="your@email.com"{...register("email", { required: true })} />
+    {errors.emailRequired && <span>Please senter email address.</span>}
+      <p>What would you lke to contribute?</p>
+    <input defaultValue="Tell us your suggestions" {...register("comment")} />
+
+      <input type="submit" />
+    </form>
     </>
   );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -14,13 +14,17 @@ function Signup() {
   
 // data is formatted in this way because it could be extended to build a mailing list in Mailchimp for exampls
   return (
+    <div>
+      <h1 className='FormHeading'> Do you want to receive news updates regularly?</h1>
+      <h2>Register your interest using the form below</h2>
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-      <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80, pattern: /^[a-z ,.'-]+$/i/i})} />
-      <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100, pattern: /^[a-z ,.'-]+$/i/i})} />
+      <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80, pattern: /^[a-z ,.'-]+$/i})} />
+      <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100, pattern: /^[a-z ,.'-]+$/i})} />
       <input type="checkbox" placeholder="Do you wish to receive regular news updates?" {...register("Do you wish to receive regular news updates?", {})} />
       <input type="submit" />
     </form>
+    </div>
   );
 }
 
