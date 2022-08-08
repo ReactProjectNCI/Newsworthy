dbb8bfc11c390fa292852437acbc340de7de80dc
Joseph Walsh <josephwalsh@arista.com>
2022-08-08 10:27:27 +0100

    header caught in loop - cannot fix

---
+++ src/components/WorldNews copy.js
@@ -0,0 +1,33 @@
+import "../structure.css";
+
+function WorldNews() {
+  return (
+    <>
+      <div className="header">
+        <h1 className="brand-header">World News</h1>
+        <h3 className="subtitle-header">
+          Your Definitive Guide to an Uncertain World
+        </h3>
+      </div>
+      <div className="WN-container">
+        <div className="main-news">
+          <h1>Breaking News</h1>
+          <p>This section contains an API with top world news stories.</p>
+          <p>One big story spanning the width of the div.</p>
+          <p>Two stories spanning the div</p>
+          <p>Remainder is four stories spanning the div</p>
+          <h3>It would be great if we can have pictures in the API</h3>
+        </div>
+        <div className="most-read">
+          <h2>Most Read</h2>
+          <p>
+            Some other API call made here - I've suggested Most Read - but that
+            may be hard to do. We could do videos or some other topic.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default WorldNews;
--- src/components/WorldNews.js
+++ src/components/WorldNews.js
@@ -1,51 +1,67 @@
 import React from "react";
 import "../structure.css";
 
-class WorldNews() extends React.Component {
+class WorldNews extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {name1: "Loading ⟳", count: 0}
+    super(props);
+    this.state = { name1: "Loading ⟳", count: 0 };
   }
-async componentDidMount() {
-    let res = await fetch(`https://newsapi.org/v2/everything?q=${this.props.newsName}&apiKey=79156c88061c41f48410cf961aa05af9`)
+  async componentDidMount() {
+    let res = await fetch(
+      `https://newsapi.org/v2/everything?q=${this.props.newsName}&apiKey=79156c88061c41f48410cf961aa05af9`
+    );
     let data = await res.json();
     let w = { width: "400px" };
     let arr = data.articles.map((p) => {
       return (
         <>
-
+          <div className="header">
+            <h1 className="brand-header">World News</h1>
+            <h3 className="subtitle-header">
+              Your Definitive Guide to an Uncertain World
+            </h3>
+          </div>
           <div className="WN-container">
-              <div className="main-news">
-              <div className="header">
+            <div className="main-news">
+              <h1>Top Stories</h1>
+              <div>
                 <div>
-                  <img src={p.urlToImage}/>
+                  <img src={p.urlToImage} />
                   <div>
-                    <div className="brand-header">{p.title}</div>
-                    <h4 class="text-gray-700 text-base" className="subtitle-header">{p.description}</h4>
-                    <button> <a href={p.url}>Read more</a></button>
+                    <div>{p.title}</div>
+                    <h4
+                      class="text-gray-700 text-base"
+                      className="subtitle-header"
+                    >
+                      {p.description}
+                    </h4>
+                    <button>
+                      {" "}
+                      <a href={p.url}>Read more</a>
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
-              <div className="most-read">
+            <div className="most-read">
               <h2>Hashtags</h2>
-                  <span>#worldsnews</span><br/>
-                  <span>#topstories</span><br/>
-                  <span>#globalnews</span><br/>
+              <span>#worldsnews</span>
+              <br />
+              <span>#topstories</span>
+              <br />
+              <span>#globalnews</span>
+              <br />
             </div>
           </div>
         </>
       );
-    })
+    });
     console.log(arr);
     this.setState({ name1: arr });
   }
   render() {
-    return <div>{this.state.name1}</div>
+    return <div>{this.state.name1}</div>;
   }
-
-
-  
 }
 
 export default WorldNews;
