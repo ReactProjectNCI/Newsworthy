2e093ef89a3307b647d89c0c428330e1f4c9a4b8
Joseph Walsh <josephwalsh@arista.com>
2022-08-09 20:05:08 +0100

    no co-ords component updated

--- src/components/NoCoOrds.js
+++ src/components/NoCoOrds.js
@@ -1,9 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlassLocation } from "@fortawesome/free-solid-svg-icons";
+
 import "../structure.css";
 
 function NoCoOrds() {
   return (
     <>
       <div className="error">
+        <br />
+        <br />
+        <FontAwesomeIcon
+          icon={faMagnifyingGlassLocation}
+          className="location-icon"
+        ></FontAwesomeIcon>
         <h1>Whoops!</h1>
         <p>
           In order for us to deliver local news, we need to know where you are.
