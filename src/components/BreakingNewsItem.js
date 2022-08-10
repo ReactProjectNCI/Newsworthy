6a86afb6bba08adb77d7d0f558c22be6ceed34a1
Joseph Walsh <josephwalsh@arista.com>
2022-08-10 21:11:53 +0100

    breaking news structure

--- src/components/BreakingNewsItem.js
+++ src/components/BreakingNewsItem.js
@@ -1,18 +1,42 @@
 import "../structure.css";
 
 function BreakingNewsItem(props) {
-  if (props.number <= 2) {
+  if (props.number === 0) {
     return (
       <>
-        <a href={props.url}>{props.title}</a>
-        <br />
-        <img className="BN-image" src={props.image} alt="" />
+        <h2 className="BN-item-title">Happening Right Now</h2>
+        <li>
+          <a href={props.url}>{props.title}</a>
+          <br />
+          <img className="BN-image" src={props.image} alt="" />
+        </li>
+      </>
+    );
+  } else if (props.number <= 2) {
+    return (
+      <>
+        <li>
+          <a href={props.url}>{props.title}</a>
+          <br />
+          <img className="BN-image" src={props.image} alt="" />
+        </li>
+      </>
+    );
+  } else if (props.number === 3) {
+    return (
+      <>
+        <h2 className="BN-item-title">Earlier in the Day</h2>
+        <li>
+          <a href={props.url}>{props.title}</a>
+        </li>
       </>
     );
   } else {
     return (
       <>
-        <a href={props.url}>{props.title}</a>
+        <li>
+          <a href={props.url}>{props.title}</a>
+        </li>
       </>
     );
   }
